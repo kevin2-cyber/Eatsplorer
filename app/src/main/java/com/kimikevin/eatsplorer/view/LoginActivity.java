87749fcfe16be1b24973c4d5e85b910d1dfa6ce5
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-07-07 17:32:32 +0000

    done with LoginActivity

--- app/src/main/java/com/kimikevin/eatsplorer/view/LoginActivity.java
+++ app/src/main/java/com/kimikevin/eatsplorer/view/LoginActivity.java
@@ -3,17 +3,37 @@ package com.kimikevin.eatsplorer.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.kimikevin.eatsplorer.R;
 import com.kimikevin.eatsplorer.databinding.ActivityLoginBinding;
 
+import java.util.Objects;
+
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
+    String email = "";
+    String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
+
+        binding.btnSignIn.setOnClickListener(view -> {
+            email = binding.etEmail.getText().toString().trim();
+            password = binding.etPass.getText().toString().trim();
+
+            if (Objects.equals(email, "kimikevin@zoho.com") && password.equals("asdfzxcvbnm")) {
+                Toast.makeText(this, "Well done", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
