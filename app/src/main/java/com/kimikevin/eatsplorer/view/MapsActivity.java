3738fa8b8aaf0421ad6d5c7b521bb0eeaced53da
Kevin2-cyber <kieduful47@gmail.com>
2025-09-12 20:39:05 +0000

    chain access

--- app/src/main/java/com/kimikevin/eatsplorer/view/MapsActivity.java
+++ app/src/main/java/com/kimikevin/eatsplorer/view/MapsActivity.java
@@ -102,9 +102,8 @@ public class MapsActivity extends AppCompatActivity
             return;
         }
 
-        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
-                Manifest.permission.ACCESS_FINE_LOCATION) || PermissionUtils
-                .isPermissionGranted(permissions, grantResults,
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)
+                || PermissionUtils.isPermissionGranted(permissions, grantResults,
                         Manifest.permission.ACCESS_COARSE_LOCATION)) {
             // Enable the my location layer if the permission has been granted.
             enableMyLocation();
