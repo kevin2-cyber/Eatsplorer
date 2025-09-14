da79403a1cda78e025ea861146e3c7336a3a9f85
Kevin2-cyber <kieduful47@gmail.com>
2025-09-14 20:42:50 +0000

    main

--- app/src/main/java/com/kimikevin/eatsplorer/view/MapsActivity.java
+++ app/src/main/java/com/kimikevin/eatsplorer/view/MapsActivity.java
@@ -105,7 +105,8 @@ public class MapsActivity extends AppCompatActivity
 
         if (PermissionUtils
                 .isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)
-                || PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                || PermissionUtils
+                .isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_COARSE_LOCATION)) {
             // Enable the my location layer if the permission has been granted.
             enableMyLocation();
         } else {
