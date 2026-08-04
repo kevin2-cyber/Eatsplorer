6dbf03e0b4f38357ee17986079a90abae958393e
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-08-04 18:13:36 +0000

    chore: update app configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.plugin.compose) apply false
     alias(libs.plugins.google.gms.google.services) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
